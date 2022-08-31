2c6d8bfc35e4bd027a97e8b3de542ead3f43224f
Victoria LeBel <vickileb@hotmail.com>
2022-08-31 08:04:05 -0700

    search set up. React router initiated

---
+++ src/components/LocationSearch.js
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react'
+
+function LocationSearch() {
+    useEffect(() => {
+        const autocomplete = new window.google.maps.places.Autocomplete(document.getElementById('google-autocomplete'), {
+            types: ('regions'),
+            componentRestrictions: {'country':['US', 'CA']},
+            fields: ['place_id','formatted_address']
+        })
+
+        autocomplete.addListener('place_changed', e => {
+            console.log(autocomplete.getPlace().formatted_address)
+        })
+        }, [])
+    return (
+    <div>
+        <h1>Location Search Bar</h1>
+        <input id="google-autocomplete" type="text" placeholder='Search'/>
+    </div>
+    )
+}
+
+export default LocationSearch
---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LocationSearch from '../components/LocationSearch'
+
+function Home() {
+  return (
+    <div>
+        <h1>Welcome</h1>
+        <LocationSearch/>
+    </div>
+  )
+}
+
+export default Home
